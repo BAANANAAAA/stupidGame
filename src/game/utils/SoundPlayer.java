7534ef2979e659bc19a852b36eff46022110357d
nomosofa <jiec_tech@hotmail.com>
2024-05-03 16:27:19 +0800

    feat(audio): implement start time for SoundPlayer

--- src/game/utils/SoundPlayer.java
+++ src/game/utils/SoundPlayer.java
@@ -9,12 +9,16 @@ import java.io.IOException;
  * @version 1.0
  */
 public class SoundPlayer {
-    public static void playSound(String filePath) {
+    public static void playSound(String filePath, long startTime) {
         try {
             File audioFile = new File(filePath);
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+            AudioFormat format = audioStream.getFormat();
+            long frames = (long) (startTime * format.getFrameRate() / 1000000); // 将毫秒转换为帧数
+
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
+            clip.setFramePosition((int) frames); // 设置起始位置
             clip.start();
         } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
             e.printStackTrace();
