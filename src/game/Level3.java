bc52e65208651123f6a72490020132715dd5d94d
zhangyihan <zhangyihan@sjtu.edu.cn>
2024-05-04 11:36:07 +0800

    feat: add a stupid clickable sofa in Level3

--- src/game/Level3.java
+++ src/game/Level3.java
@@ -1,6 +1,9 @@
 package game;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Level3 extends Level {
 
@@ -18,5 +21,32 @@ public class Level3 extends Level {
         JLabel label = new JLabel(imageIcon);
         label.setBounds(0, 0, frame.getWidth(), frame.getHeight()); // 覆盖整个窗口
         layeredPane.add(label, Integer.valueOf(1)); // 添加至低层
+
+        createSofaHintArea();
+    }
+
+    private void createSofaHintArea() {
+        int sofaWidth = 100; // Width of the clickable area
+        int sofaHeight = 50; // Height of the clickable area
+        int sofaX = frame.getWidth() - sofaWidth - 30; // Position X (from right minus width of the area and some margin)
+        int sofaY = frame.getHeight() - sofaHeight - 30; // Position Y (from bottom minus height of the area and some margin)
+
+        JLabel sofaArea = new JLabel();
+        sofaArea.setBounds(sofaX, sofaY, sofaWidth, sofaHeight);
+        sofaArea.setCursor(new Cursor(Cursor.HAND_CURSOR)); // Change the cursor to indicate clickable area
+
+        // Add mouse listener to show hint when clicked
+        sofaArea.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                showHint();
+            }
+        });
+
+        layeredPane.add(sofaArea, Integer.valueOf(2)); // Add the clickable area above the background
+    }
+
+    private void showHint() {
+        JOptionPane.showMessageDialog(frame, "Look behind the mirror", "Hint", JOptionPane.INFORMATION_MESSAGE);
     }
 }
