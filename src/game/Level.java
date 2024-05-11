135e68df4538f1c8adc8a56b66d849f1f325fcab
XuanyuYang <yangxuanyu03@163.com>
2024-05-11 08:38:52 +0800

    feat: Add level movement structure.

--- src/game/Level.java
+++ src/game/Level.java
@@ -3,6 +3,8 @@ package game;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 abstract public class Level extends JPanel {
     public JFrame frame; // 所有level共享一个显示frame
@@ -11,6 +13,8 @@ abstract public class Level extends JPanel {
 
     public static final int contentWidth = 800;
     public static final int contentHeight = 750;
+    private static final int BORDER_WIDTH = 60;
+
 
     public Level(JFrame mainFrame, Player player) {
         this.frame = mainFrame;
@@ -18,9 +22,36 @@ abstract public class Level extends JPanel {
         layeredPane = new JLayeredPane();
         layeredPane.setPreferredSize(new Dimension(contentWidth, contentHeight));
         frame.add(layeredPane, BorderLayout.EAST);
+
+        layeredPane.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int x = e.getX();
+                int y = e.getY();
+                System.out.println(x + " " + y);
+                if (y <= BORDER_WIDTH) {
+                    System.out.println("up");
+                    goUp();
+                } else if (y >= contentHeight - BORDER_WIDTH) {
+                    System.out.println("down");
+                    goDown();
+                } else if (x >= contentWidth - BORDER_WIDTH) {
+                    System.out.println("right");
+                    goRight();
+                } else if (x <= BORDER_WIDTH) {
+                    System.out.println("left");
+                    goLeft();
+                }
+            }
+        });
     }
 
     abstract void init(); // 函数应该设置所有的ui图片和listener，放进layeredPane
+
+    abstract void goUp();
+    abstract void goDown();
+    abstract void goRight();
+    abstract void goLeft();
 
     public JLayeredPane getLayeredPane() {
         return layeredPane;
